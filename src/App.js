0b64cee35614832a30f39762ac720005224a6b7e
katherinestout <kjs14@hood.edu>
2018-10-22 22:38:29 -0400

    help

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+
+//store the todos in the state
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play some mario kart'}
+    ]
+  }
+  //delete functionality
+  deleteTodo = (id) => {
+    //console.log(id);
+  //if this todo id is not equal to that id then return true
+  //if they are equal than it is going to delete it
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+          <h1>Todo's</h1>
+          <Todos todos = {this.state.todos} 
+          deleteTodo = {this.deleteTodo}/>
       </div>
     );
   }
