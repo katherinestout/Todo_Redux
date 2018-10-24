40f942aab00eb5a7aa9ab7c1dadbe8a2e0e5aa01
katherinestout <kjs14@hood.edu>
2018-10-24 13:50:55 -0400

    Updates

---
+++ precache-manifest.28e630336f4c02de227d32fa889b6d88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "f07837ef261ee1066801",
+    "url": "/my-app/static/js/main.f07837ef.chunk.js"
+  },
+  {
+    "revision": "2345088808eca074ace8",
+    "url": "/my-app/static/js/1.23450888.chunk.js"
+  },
+  {
+    "revision": "f07837ef261ee1066801",
+    "url": "/my-app/static/css/main.ca35f6c2.chunk.css"
+  },
+  {
+    "revision": "d7abfba2359f55067cb4081ef7048d87",
+    "url": "/my-app/index.html"
+  }
+];
