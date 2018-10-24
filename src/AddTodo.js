70e74d2c0e59eb9797f5e96feb33491993dfd940
katherinestout <kjs14@hood.edu>
2018-10-24 12:36:03 -0400

    clearing input field

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -10,19 +10,25 @@ class AddTodo extends Component {
            content: e.target.value
        })
     }
-    
+
     handleSubmit = (e) => {
         e.preventDefault();
        // console.log(this.state);
        //passing content into this function
        this.props.addTodo(this.state);
+       //clearing the input, resetting the state..adding value to input
+       this.setState({
+           content: ''
+       })
     }
+
     render(){
         return (
             <div>
                 <form onSubmit = {this.handleSubmit}>
                 <label>Add new Todo:</label>
-                <input type ="text" onChange={this.handleChange}/>
+                <input type ="text" onChange={this.handleChange} 
+                value ={this.state.content}/>
                 </form>
             </div>
         )
